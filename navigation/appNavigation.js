55205678998e56c03f0e1c952eabfdd502089174
Carl Mari Datangel <datangel.carl0504@gmail.com>
2024-09-25 13:13:10 +0800

    wala lang

--- navigation/appNavigation.js
+++ navigation/appNavigation.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator} from '@react-navigation/stack';
 import LoginScreen from '../screens/LoginScreen'; // Login page
 import SignupScreen from '../screens/SignupScreen'; // Signup page
+import WelcomeScreen from '../screens/WelcomeScreen';
 
 const Stack = createStackNavigator();
 
 const AppNavigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false}}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name='Welcome' component={WelcomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
